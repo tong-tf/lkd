3f65617e173be90a14b7e723026cf2001e5462bc
tong <tj_wust@163.com>
2019-03-10 22:11:07 +0800

    test.c add test_list

--- c6/test.c
+++ c6/test.c
@@ -2,14 +2,18 @@
 #include <linux/init.h>
 
 #include "common.h"
+#include "test_list.h"
+
+
 
 static int __init test_init(void)
 {
 	LOG("Called\n");
+	test_list();
 	return 0;
 }
 
-static void __init test_exit(void)
+static void __exit test_exit(void)
 {
 	LOG("called\n");
 }
@@ -17,4 +21,4 @@ static void __init test_exit(void)
 module_init(test_init);
 module_exit(test_exit);
 MODULE_AUTHOR("tong");
-MODULE_LICENSE("GPLv2");
+MODULE_LICENSE("GPL");
